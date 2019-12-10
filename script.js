40a69018eb6e5b29571fabd086bdccb7bba376ba
pgfckr <smuelle1@stud.hs-heilbronn.de>
2019-12-10 12:03:32 +0100

    added buggy linebreaker function

--- script.js
+++ script.js
@@ -5,18 +5,63 @@
  * 
  */
 
-function convert(){
+function convert() {
     var raw = document.getElementById("inputArea").value;
-    var ref = findBreaks(raw);
+    var ref = findBreaks(raw.replace(/\"/g, "''"));
+    ref = makeAlias(ref);
     document.getElementById("outputArea").value = ref;
-
 }
 
-function findBreaks(raw){
+/**
+ * Breaks the input into strings of a maximum lenght
+ * the variance is the space in one of those strings where the script will look for the rightmost linebreaks colons or semicolons 
+ * those ore considered better breakpoints than splitting whole words
+ * @param {*} rawString Input from the textarea - " symbol
+ * @returns array of strings
+ */
+
+function findBreaks(rawString) {
+    var rawInput = rawString;
     var ref = [];
-    ref.push("line");
-    ref.push(raw);
-    ref.push("line")
+    var maxChars = 20;
+    var maxVariation = 10;
+
+    while (rawInput.length > maxChars) {
+        
+        var tempString = rawInput.substring(0, maxChars);
+        var bestBreakpoint = maxChars;
+        var criticalArea = rawInput.substring(maxChars - maxVariation, maxChars);
+
+        if (criticalArea.includes("\n")) {
+            ref.push(rawInput.substring(0, rawInput.lastIndexOf("\n")));
+            rawInput = rawInput.substring(rawInput.lastIndexOf("\n"));
+        }
+
+        else if (criticalArea.includes(".")) {
+            ref.push(rawInput.substring(0, rawInput.lastIndexOf(".")));
+            rawInput = rawInput.substring(rawInput.lastIndexOf("."));
+        }
+
+        else if (criticalArea.includes(" ")) {
+            ref.push(rawInput.substring(0, rawInput.lastIndexOf(" ")));
+            rawInput = rawInput.substring(rawInput.lastIndexOf(" "));
+        }
+
+        else {
+            ref.push(rawInput.substring(0, maxChars));
+            rawInput = rawInput.substring(maxChars);
+        }
+
+    }
+
+    if (rawInput.length > 0) {
+        ref.push(rawInput);
+    }
     return ref;
+}
+
+function makeAlias(stringArray) {
+    var aliasString = stringArray.length;
+    return aliasString;
 }
 
